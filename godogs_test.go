4aa06a2ded88259e9694ee2c6e438e862fff8dba
greeflas <vldmr.kuprienko@gmail.com>
2023-02-16 22:45:16 +0200

    Step definitions logic

--- godogs_test.go
+++ godogs_test.go
@@ -1,13 +1,61 @@
 package godogs
 
-import "github.com/cucumber/godog"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/cucumber/godog"
+	"testing"
+)
 
-func iEat(arg1 int) {}
+// godogsCtxKey is the key used to store the available godogs in the context.Context.
+type godogsCtxKey struct{}
 
-func thereAreGodogs(arg1 int) {}
+func thereAreGodogs(ctx context.Context, available int) context.Context {
+	return context.WithValue(ctx, godogsCtxKey{}, available)
+}
 
-func thereShouldBeRemaining(arg1 int) error {
+func iEat(ctx context.Context, num int) (context.Context, error) {
+	available, ok := ctx.Value(godogsCtxKey{}).(int)
+	if !ok {
+		return ctx, errors.New("there are no godogs available")
+	}
+
+	if available < num {
+		return ctx, fmt.Errorf("you cannot eat %d godogs, there are %d available", num, available)
+	}
+
+	available -= num
+
+	return context.WithValue(ctx, godogsCtxKey{}, available), nil
+}
+
+func thereShouldBeRemaining(ctx context.Context, remaining int) error {
+	available, ok := ctx.Value(godogsCtxKey{}).(int)
+	if !ok {
+		return errors.New("there are no godogs available")
+	}
+
+	if available != remaining {
+		return fmt.Errorf("expected %d godogs to be remaining, but there is %d", remaining, available)
+	}
+
 	return nil
+}
+
+func TestFeatures(t *testing.T) {
+	suite := godog.TestSuite{
+		ScenarioInitializer: InitializeScenario,
+		Options: &godog.Options{
+			Format:   "pretty",
+			Paths:    []string{"features"},
+			TestingT: t,
+		},
+	}
+
+	if suite.Run() != 0 {
+		t.Fatal("non-zero status returned, failed to run feature tests")
+	}
 }
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
